8b9f1509b158045cd193358048360aacf47728bd
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-06-02 23:33:41 +0200

    template for table.

--- app/controllers/Tables.scala
+++ app/controllers/Tables.scala
@@ -7,5 +7,12 @@ import play.api.data.Forms._
 import models._
 
 object Tables extends Controller {
-  def show(factName: String, d1: String, d2: String) = TODO
+  def show(factName: String, d1Name: String, d2Name: String) = Action {
+    val r = for {
+      fact ← Fact.find(factName)
+      d1 ← fact.dimensions.find(_.name == d1Name)
+      d2 ← fact.dimensions.find(_.name == d2Name)
+    } yield Ok(views.html.table(fact, d1, d2))
+    r.getOrElse(NotFound)
+  }
 }

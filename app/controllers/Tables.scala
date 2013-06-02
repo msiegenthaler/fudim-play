f82820d00d534d1eef0d9ac218f0c1225324648c
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-06-02 23:28:39 +0200

    add Tables controller.

---
+++ app/controllers/Tables.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models._
+
+object Tables extends Controller {
+  def show(factName: String, d1: String, d2: String) = TODO
+}

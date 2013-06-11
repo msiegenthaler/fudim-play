bb9de52ec67ab20acc3688a89fce9205f960c36a
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-06-11 21:19:45 +0200

    fix warning.

--- app/support/JsHelper.scala
+++ app/support/JsHelper.scala
@@ -10,7 +10,7 @@ object JsHelper {
   /** Pendant to window.fudim.point.parse */
   def serializePoint(p: Point): String = {
     val json = Json.toJson(p.values.map(e => (e._1.name, e._2)))
-    URLEncoder.encode(json.toString)
+    URLEncoder.encode(json.toString, "UTF-8")
   }
   
 }

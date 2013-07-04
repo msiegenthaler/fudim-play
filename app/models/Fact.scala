2a53249dd1204eebf0a8ed2312e66a6d1e0be75b
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-07-04 23:51:31 +0200

    add sum-aggregation for facts.

--- app/models/Fact.scala
+++ app/models/Fact.scala
@@ -1,5 +1,6 @@
 package models
 
+import util.control.Exception._
 import anorm._
 import anorm.SqlParser._
 import play.api.db._
@@ -25,28 +26,28 @@ sealed trait Fact {
 
 /** Fact that is backed by a database store for all fully defined points. */
 sealed trait DatabaseBackedFact extends Fact {
-  val cube: DatabaseCube[String]
+  val cube: EditableCube[String]
   override def dimensions = cube.dimensions
   override def get(at: Point) = cube.get(at)
   override def set(at: Point, value: Option[String]) = cube.set(at, value)
   override def canSet(at: Point) = cube.isSettable(at)
 }
 
-private case class DatabaseFact(name: String, cube: DatabaseCube[String]) extends DatabaseBackedFact
+private case class DatabaseFact(name: String, cube: EditableCube[String]) extends DatabaseBackedFact
 
 object Fact {
   def get(name: String): Option[Fact] = DB.withConnection { implicit c ⇒
     for {
       id ~ name ~ cubeId ← SQL("select * from fact where name={name}").on("name" -> name).as(long("id") ~ str("name") ~ long("cube") singleOpt)
       cube ← DatabaseCube.load(cubeId, classOf[String])
-    } yield (DatabaseFact(name, cube))
+    } yield (DatabaseFact(name, aggregatable(cube)))
   }
   def all: Iterable[Fact] = DB.withConnection { implicit c ⇒
     for {
       value ← SQL("select * from fact").as(long("id") ~ str("name") ~ long("cube") *)
       id ~ name ~ cubeId = value
       cube ← DatabaseCube.load(cubeId, classOf[String])
-    } yield DatabaseFact(name, cube)
+    } yield DatabaseFact(name, aggregatable(cube))
   }
 
   def create(name: String, dims: Set[Dimension]): Fact = DB.withConnection { implicit c ⇒
@@ -54,5 +55,15 @@ object Fact {
     val fact = DatabaseFact(name, cube)
     SQL("insert into fact(name, cube) values({name}, {cube})").on("name" -> name, "cube" -> cube.id).executeInsert().get
     fact
+  }
+
+  //TODO replace with something efficient, this is just for a demo
+  def aggregatable(cube: DatabaseCube[String]) = AggregateCube(cube, Aggregators.fold(Some("0"))(sumIfNumber))
+  private def sumIfNumber(oa: Option[String], b: String): Option[String] = {
+    for {
+      a ← oa
+      na ← catching(classOf[NumberFormatException]).opt(a.toLong)
+      nb ← catching(classOf[NumberFormatException]).opt(b.toLong)
+    } yield (na + nb).toString
   }
 }

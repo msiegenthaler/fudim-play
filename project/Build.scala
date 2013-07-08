9b382e813bea0e914c9463db4dd2154bf12cbd60
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-07-08 23:47:28 +0200

    heroku config.

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,7 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
     anorm)
 
   val main = play.Project(appName, appVersion, appDependencies).settings(

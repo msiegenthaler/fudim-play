dd513687038bd540164e66535a302536030f9e44
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-08-11 15:45:28 +0200

    rename dimension in CubeTck.

--- test/cube/CubeSpec.scala
+++ test/cube/CubeSpec.scala
@@ -15,93 +15,93 @@ trait CubeTck extends Specification with DataTables {
    * One-Dimensional cube containing the fact "Mario's age" over the year-Dimension.
    * Data: (2013 -> 32, 2014 -> 33, 2015 -> 34)
    */
-  def oneD: Cube[Int]
+  def age: Cube[Int]
 
   s"$name one-dimensional" should {
     "contain 3 values (values, sparse, dense)" in {
-      oneD.values must have size 3
-      oneD.sparse must have size 3
-      oneD.dense must have size 3
+      age.values must have size 3
+      age.sparse must have size 3
+      age.dense must have size 3
     }
     "have get return the correct value for each year " ! {
       "year" || "age" |
         "2013" !! 32 |
         "2014" !! 33 |
-        "2015" !! 34 |> { (y, age) ⇒
+        "2015" !! 34 |> { (y, a) ⇒
           val at = year.coordOf(y)
-          oneD.get(at) must_== Some(age)
-          oneD.isDefinedAt(at) must beTrue
-          oneD(at) must_== age
+          age.get(at) must_== Some(a)
+          age.isDefinedAt(at) must beTrue
+          age(at) must_== a
         }
     }
     "have year as its one and only dimension" in {
-      oneD.dimensions must_== Set(year)
+      age.dimensions must_== Set(year)
     }
     "return itself as .raw" in {
-      oneD.raw must_== oneD
+      age.raw must_== age
     }
     "have an empty slice" in {
-      oneD.slice must_== Point.empty
+      age.slice must_== Point.empty
     }
   }
   s"$name one-dimensional sliced to 2014" should {
-    def oneDS = oneD.slice(Point(year.coordOf("2014")))
+    def ageS = age.slice(Point(year.coordOf("2014")))
     "have slice Point(2014)" in {
-      oneDS.slice must_== Point(year.coordOf("2014"))
+      ageS.slice must_== Point(year.coordOf("2014"))
     }
     "have no dimension" in {
-      oneDS.dimensions must be empty
+      ageS.dimensions must be empty
     }
     "contain one value (values, sparse, dense)" in {
-      oneDS.values must have size 1
-      oneDS.sparse must have size 1
-      oneDS.dense must have size 1
+      ageS.values must have size 1
+      ageS.sparse must have size 1
+      ageS.dense must have size 1
     }
     "return 33 for get(2014)" in {
       val at = year.coordOf("2014")
-      oneDS.get(at) must_== Some(33)
-      oneDS.isDefinedAt(at) must beTrue
-      oneDS(at) must_== 33
+      ageS.get(at) must_== Some(33)
+      ageS.isDefinedAt(at) must beTrue
+      ageS(at) must_== 33
     }
     "be undefined at 2013, 2015" in {
-      oneDS.isDefinedAt(year.coordOf("2013")) must beFalse
-      oneDS.isDefinedAt(year.coordOf("2015")) must beFalse
+      ageS.isDefinedAt(year.coordOf("2013")) must beFalse
+      ageS.isDefinedAt(year.coordOf("2015")) must beFalse
     }
     "raw return the original cube" in {
-      oneDS.raw must_== oneD
+      ageS.raw must_== age
     }
     "raw have 3 values" in {
-      oneDS.raw.values must have size 3
+      ageS.raw.values must have size 3
     }
   }
   s"$name one-dimensional diced to 2014" should {
-    def oneDD = oneD.dice(year, _ == year.coordOf("2014"))
+    def ageD = age.dice(year, _ == year.coordOf("2014"))
     "have one dimension (year)" in {
-      oneDD.dimensions must_== Set(year)
+      ageD.dimensions must_== Set(year)
     }
     "have empty slice" in {
-      oneDD.slice must_== Point.empty
+      ageD.slice must_== Point.empty
     }
     "contain one value (values, sparse, dense)" in {
-      oneDD.values must have size 1
-      oneDD.sparse must have size 1
-      oneDD.dense must have size 1
+      ageD.values must have size 1
+      ageD.sparse must have size 1
+      ageD.dense must have size 1
     }
     "return 33 for get(2014)" in {
       val at = year.coordOf("2014")
-      oneDD.get(at) must_== Some(33)
-      oneDD.isDefinedAt(at) must beTrue
-      oneDD(at) must_== 33
+      ageD.get(at) must_== Some(33)
+      ageD.isDefinedAt(at) must beTrue
+      ageD(at) must_== 33
     }
     "be undefined at 2013, 2015" in {
-      oneDD.isDefinedAt(year.coordOf("2013")) must beFalse
-      oneDD.isDefinedAt(year.coordOf("2015")) must beFalse
+      ageD.isDefinedAt(year.coordOf("2013")) must beFalse
+      ageD.isDefinedAt(year.coordOf("2015")) must beFalse
     }
     "raw return the original cube" in {
-      oneDD.raw must_== oneD
+      ageD.raw must_== age
     }
     "raw have 3 values" in {
-      oneDD.raw.values must have size 3
+      ageD.raw.values must have size 3
     }
   }
 }
@@ -110,7 +110,7 @@ class MapCubeSpec extends Specification {
   include(new CubeTck {
     val name = "MapCube"
     import Dimensions._
-    val oneD = {
+    val age = {
       val data = Map("2013" -> 32, "2014" -> 33, "2015" -> 34)
       MapCube(data.map(v ⇒ (Point(year.coordOf(v._1)), v._2)))
     }

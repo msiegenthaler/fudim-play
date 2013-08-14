ae43b31f52c6b6b6644aea8d73769bdadd765ce4
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-08-14 20:48:47 +0200

    update play to 2.1.3.

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName = "fudim_play"
-  val appVersion = "1.1"
+  val appVersion = "dev-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,

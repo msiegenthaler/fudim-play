96768dacfaf29962e8fd7a048fe05c1bff7a3a32
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-08-14 19:02:04 +0200

    use scope style before/after in DatabaseCubeSpec.

--- test/models/dbcube/DatabaseCubeSpec.scala
+++ test/models/dbcube/DatabaseCubeSpec.scala
@@ -1,7 +1,7 @@
 package models.dbcube
 
 import org.specs2.mutable._
-import org.specs2.specification.BeforeAfterExample
+import org.specs2.specification.{ Scope, BeforeAfterExample }
 import play.api.Play
 import play.api.test._
 import play.api.test.Helpers._
@@ -10,9 +10,8 @@ import models._
 import models.dbcube._
 import Point._
 
-class DatabaseCubeSpec extends Specification with BeforeAfterExample {
+class DatabaseCubeSpec extends Specification {
   include(new CubeTck("DatabaseCube") with BeforeAfterExample {
-
     override def before = Play.start(FakeApplication())
     override def after = Play.stop
 
@@ -36,52 +35,48 @@ class DatabaseCubeSpec extends Specification with BeforeAfterExample {
     }
   })
 
-  def monat = Dimension.get("Monat").get
-  def jan = monat.all(0)
-  def feb = monat.all(1)
-  def mar = monat.all(2)
+  trait withplay extends Scope with BeforeAfter {
+    override def before = Play.start(FakeApplication())
+    override def after = Play.stop
+  }
+  trait oneDimensionalCube extends withplay {
+    lazy val monat = Dimension.get("Monat").get
+    def jan = monat.all(0)
+    def feb = monat.all(1)
+    def mar = monat.all(2)
+    lazy val cube = DatabaseCube.create(Set(monat), classOf[String])
+  }
 
-  def oneDimensional = DatabaseCube.create(Set(monat), classOf[String])
-
-  override def before = Play.start(FakeApplication())
-  override def after = Play.stop
-
-  "DatabaseCube with one dimension" should {
-    "be droppable" in {
-      val cube = oneDimensional
+  "DatabaseCube of type String with one dimension" should {
+    "be droppable" in new oneDimensionalCube {
       DatabaseCube.delete(cube)
+      DatabaseCube.load(cube.id) must beNone
     }
-    "be droppable if it has values" in {
-      val cube = oneDimensional
+    "be droppable if it has values" in new oneDimensionalCube {
       cube.set(jan, Some("1"))
       cube.set(feb, Some("2"))
       DatabaseCube.delete(cube)
     }
-    "be initialized with all None" in {
-      val cube = oneDimensional
+    "be initialized with all None" in new oneDimensionalCube {
       cube.dense.foreach(v ⇒ v._2 must beNone)
     }
-    "return the set value" in {
-      val cube = oneDimensional
+    "return the set value" in new oneDimensionalCube {
       cube.set(jan, Some("1"))
       cube.get(jan) must equalTo(Some("1"))
     }
-    "have all the value in all fields if set with setAll" in {
-      val cube = oneDimensional
+    "have all the value in all fields if set with setAll" in new oneDimensionalCube {
       cube.setAll(Some("X"))
       cube.dense.foreach(v ⇒ v._2 must beSome("X"))
       monat.all.foreach(p ⇒ cube.get(p) must beSome("X"))
     }
-    "have sparse values only for set fields" in {
-      val cube = oneDimensional
+    "have sparse values only for set fields" in new oneDimensionalCube {
       cube.set(jan, Some("X"))
       cube.set(mar, Some("Y"))
       cube.values.toSet must equalTo(Set("X", "Y"))
       val exp: Set[(Point, String)] = Set((jan, "X"), (mar, "Y"))
       cube.sparse.toSet must equalTo(exp)
     }
-    "be the same when loaded" in {
-      val cube = oneDimensional
+    "be the same when loaded" in new oneDimensionalCube {
       cube.set(jan, Some("X"))
       val cube2 = DatabaseCube.load(cube.id, classOf[String]).get
       cube2.values.toSet must equalTo(Set("X"))

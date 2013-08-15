e916415c958d19d9ccb0de422a1aaafcf99548dc
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-08-15 21:41:47 +0200

    extract some test-data into cube.TestFixtures.

--- test/cube/AggregatorSpec.scala
+++ test/cube/AggregatorSpec.scala
@@ -1,63 +1,49 @@
 package cube
 
 import org.specs2.mutable.Specification
+import TestFixtures._
 
 class AggregatorSpec extends Specification {
-  val german = ListDimension("german", "eins", "zwei", "drei")
-  val english = ListDimension("english", "one", "two", "three")
-
-  val productCube = {
-    val data = for {
-      (cg, vg) ← german.all.zipWithIndex
-      (ce, ve) ← english.all.zipWithIndex
-    } yield (Point(cg, ce), ((vg + 1) * (ve + 1)).toLong)
-    MapCube(data.toMap)
+  trait sumCube extends productCube {
+    val cube = CubeDecorator(productCube, Aggregators.sumInt)
   }
-
-  private val einsOne = Point(german.coordOf("eins"), english.coordOf("one"))
-  private val einsTwo = Point(german.coordOf("eins"), english.coordOf("two"))
-  private val zweiTwo = Point(german.coordOf("zwei"), english.coordOf("two"))
-
   "aggregation of cube with sum" should {
-    val dec = CubeDecorator(productCube, Aggregators.sum)
-
-    "have value 18 at drei" in {
-      dec.get(german.coordOf("drei")) must beSome(18)
+    "have value 18 at drei" in new sumCube {
+      cube.get(german.coordOf("drei")) must beSome(18)
     }
-    "have value 12 at zwei" in {
-      dec.get(german.coordOf("zwei")) must beSome(12)
+    "have value 12 at zwei" in new sumCube {
+      cube.get(german.coordOf("zwei")) must beSome(12)
     }
-    "have value 6 at eins" in {
-      dec.get(german.coordOf("eins")) must beSome(6)
+    "have value 6 at eins" in new sumCube {
+      cube.get(german.coordOf("eins")) must beSome(6)
     }
-    "have value 6 at one" in {
-      dec.get(english.coordOf("one")) must beSome(6)
+    "have value 6 at one" in new sumCube {
+      cube.get(english.coordOf("one")) must beSome(6)
     }
-    "have value 36 at Point.empty" in {
-      dec.get(Point.empty) must beSome(36)
+    "have value 36 at Point.empty" in new sumCube {
+      cube.get(Point.empty) must beSome(36)
     }
-    "still have 9 values" in {
-      dec.values must have size 9
-      dec.sparse must have size 9
-      dec.dense.filter(_._2.isDefined) must have size 9
+    "still have 9 values" in new sumCube {
+      cube.values must have size 9
+      cube.sparse must have size 9
+      cube.dense.filter(_._2.isDefined) must have size 9
     }
-    "still have 9 points" in {
-      dec.dense must have size 9
+    "still have 9 points" in new sumCube {
+      cube.dense must have size 9
     }
-    "have 4 at zwei/two" in {
-      dec.get(zweiTwo) must beSome(4)
+    "have 4 at zwei/two" in new sumCube {
+      cube.get(zweiTwo) must beSome(4)
     }
-    "have a value sum of 36" in {
-      dec.values.reduce(_ + _) must_== (36)
+    "have a value sum of 36" in new sumCube {
+      cube.values.reduce(_ + _) must_== (36)
     }
   }
 
   "Aggregator" should {
     "be unapplyable from Decorator" in {
-      val aggr = Aggregators.sum
-      val dec: CubeDecorator[Long] = aggr
+      val aggr = Aggregators.sumInt
+      val dec: CubeDecorator[Int] = aggr
       Aggregator.unapply(dec) must_== Some(aggr)
     }
-
   }
 }
--- test/cube/CubeDecoratorSpec.scala
+++ test/cube/CubeDecoratorSpec.scala
@@ -5,35 +5,9 @@ import org.specs2.specification.Scope
 import support.JsonMapperRepository
 import CubeDecorator._
 import support.ObjectJsonMapper
+import TestFixtures._
 
 class CubeDecoratorSpec extends Specification {
-  trait germanEnglish extends Scope {
-    val german = ListDimension("german", "eins", "zwei", "drei")
-    val english = ListDimension("english", "one", "two", "three")
-
-    val einsOne = Point(german.coordOf("eins"), english.coordOf("one"))
-    val einsTwo = Point(german.coordOf("eins"), english.coordOf("two"))
-    val zweiTwo = Point(german.coordOf("zwei"), english.coordOf("two"))
-  }
-  trait productCube extends germanEnglish {
-    val productCube = {
-      val data = for {
-        (cg, vg) ← german.all.zipWithIndex
-        (ce, ve) ← english.all.zipWithIndex
-      } yield (Point(cg, ce), (vg + 1) * (ve + 1))
-      MapCube(data.toMap)
-    }
-  }
-  trait hasDiffCube extends germanEnglish {
-    val hasDiffCube = {
-      val data = for {
-        (cg, vg) ← german.all.zipWithIndex
-        (ce, ve) ← english.all.zipWithIndex if ve != vg
-      } yield (Point(cg, ce), (vg + 1) - (ve + 1))
-      MapCube(data.toMap)
-    }
-  }
-
   trait decNoop extends productCube {
     val dec = CubeDecorator(productCube, CubeDecorators.noop[Int])
   }

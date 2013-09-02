71f3e2d5eaef1d50f19efb82d34168177b8a00bf
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-02 21:58:29 +0200

    add FudimDimensionRepo.

--- app/models/FudimDimension.scala
+++ app/models/FudimDimension.scala
@@ -8,3 +8,10 @@ trait FudimDimension extends Dimension {
   /** Adds a value to the dimension directly after another value (use None to insert as first). */
   def add(value: String, after: Option[Coordinate]): Coordinate
 }
+
+trait FudimDimensionRepo {
+  def get(domain: DomainId, name: String): Option[FudimDimension]
+  def all(): List[FudimDimension]
+  def create(domain: DomainId, name: String): FudimDimension
+  def remove(domain: DomainId, name: String): Unit
+}

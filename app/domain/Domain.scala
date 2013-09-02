bf8dae0790b582dcb1cc59b4eb28e2ae4db0794c
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-02 21:28:14 +0200

    add accessors by name.

--- app/domain/Domain.scala
+++ app/domain/Domain.scala
@@ -6,5 +6,8 @@ trait Domain {
   def name: String
 
   def dimensions: Set[Dimension]
+  def dimension(name: String): Option[Dimension]
+
   def facts: Set[Fact[_]]
+  def fact(name: String): Option[Fact[_]]
 }

fdaa4d8263a0ef3858420d70859bc87f4d45068f
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-06 16:04:35 +0200

    create the example data in the example domain.

--- app/Global.scala
+++ app/Global.scala
@@ -3,6 +3,7 @@ import play.api._
 import models._
 import cube._
 import support.JsonMapper
+import models.play.DomainRepo
 
 object Global extends GlobalSettings {
   val aggregation = Aggregation.all //so they get registered
@@ -12,28 +13,30 @@ object Global extends GlobalSettings {
 }
 
 object InitialData {
-  def hasData = !DimensionRepo.all.isEmpty
+  def hasData = !DomainRepo.get("example").isEmpty
   def insert {
-    val monat = DimensionRepo.create("Monat")
+    val example = DomainRepo.create("example")
+
+    val monat = example.dimensionRepo.create("Monat")
     monat.add("Jan")
     monat.add("Feb")
     monat.add("Mar")
     monat.add("Apr")
     monat.add("Mai")
 
-    val project = DimensionRepo.create("Projekt")
+    val project = example.dimensionRepo.create("Projekt")
     project.add("BZ")
     project.add("AB")
     project.add("GG")
 
-    val kostenart = DimensionRepo.create("Kostenart")
+    val kostenart = example.dimensionRepo.create("Kostenart")
     val ka_ma = kostenart.add("Mitarbeiter")
     val ka_ext = kostenart.add("Externe")
     val ka_mat = kostenart.add("Material")
     val ka_gk = kostenart.add("Gemeinkosten")
 
-    val umsatz = FactRepo.createDatabaseBacked("Umsatz", DataType.integer, Set(monat, project), Aggregation.sum.aggregator)
-    val kosten = FactRepo.createDatabaseBacked("Kosten", DataType.integer, Set(monat, project, kostenart), Aggregation.sum.aggregator)
+    val umsatz = example.factRepo.createDatabaseBacked("Umsatz", DataType.integer, Set(monat, project), Aggregation.sum.aggregator)
+    val kosten = example.factRepo.createDatabaseBacked("Kosten", DataType.integer, Set(monat, project, kostenart), Aggregation.sum.aggregator)
 
     val rnd = new Random(1)
     for (m ← monat.all) {

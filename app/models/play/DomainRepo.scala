0fe3d1264a21281868a5026156c5a6b32e1082c3
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-06 15:49:44 +0200

    add Play repos.

---
+++ app/models/play/DomainRepo.scala
@@ -0,0 +1,17 @@
+package models
+package play
+
+import cube._
+import db._
+import dbcube.DatabaseCubeRepo
+
+object DomainRepo extends FudimDomainRepo {
+  private object DimensionRepo extends DatabaseDimensionRepo with PlayDatabaseRepo
+  private object DbCubeRepo extends DatabaseCubeRepo with PlayDatabaseRepo
+  private object FactRepo extends DatabaseFactRepo with PlayDatabaseRepo {
+    override def databaseCubeRepo = DbCubeRepo
+    override def jsonCubeMapperRepo = new JsonCubeMapperRepository {
+      override val mappers = DbCubeRepo.json :: CubeDecorator.json(JsonMappers.decorator, this) :: Nil
+    }
+  }
+}

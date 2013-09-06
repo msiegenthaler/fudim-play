c6e8ccd6b50cdeb0f615dcbbbffa10871f73b2e5
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-06 17:02:28 +0200

    Dimension in now inside the domain (changed controller and routes).

--- app/controllers/Dimensions.scala
+++ app/controllers/Dimensions.scala
@@ -5,42 +5,42 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import models._
+import support.DomainAction
 
 object Dimensions extends Controller {
-
-  def index = Action {
-    Ok(views.html.dimensions(DimensionRepo.all, addForm))
+  def index(domain: String) = DomainAction(domain) { domain ⇒
+    Ok(views.html.dimensions(domain.dimensions, addForm))
   }
 
-  def add = Action { implicit request ⇒
+  def add(domainName: String) = DomainAction(domainName).on(domain ⇒ { implicit request ⇒
     addForm.bindFromRequest.fold(
-      errors ⇒ BadRequest(views.html.dimensions(DimensionRepo.all, errors)),
+      errors ⇒ BadRequest(views.html.dimensions(domain.dimensions, errors)),
       name ⇒ {
-        DimensionRepo.create(name)
-        Redirect(routes.Dimensions.index)
+        domain.dimensionRepo.create(name)
+        Redirect(routes.Dimensions.index(domainName))
       })
-  }
+  })
 
-  def get(name: String) = Action {
+  def get(domainName: String, name: String) = DomainAction(domainName) { domain ⇒
     val r = for {
-      d ← DimensionRepo.get(name)
+      d ← domain.dimension(name)
       vs = d.values
     } yield Ok(views.html.dimension(d, vs, addValueForm))
     r.getOrElse(NotFound)
   }
 
-  def addValue(name: String) = Action { implicit request ⇒
+  def addValue(domainName: String, name: String) = DomainAction(domainName).on(domain ⇒ { implicit request ⇒
     (for {
-      d ← DimensionRepo.get(name)
+      d ← domain.dimensionRepo.get(name)
     } yield {
       addValueForm.bindFromRequest.fold(
         errors ⇒ BadRequest(views.html.dimension(d, d.values, errors)),
         value ⇒ {
           d.add(value)
-          Redirect(routes.Dimensions.get(name))
+          Redirect(routes.Dimensions.get(domainName, name))
         })
     }).getOrElse(NotFound)
-  }
+  })
 
   val addForm = Form("name" -> nonEmptyText)
 

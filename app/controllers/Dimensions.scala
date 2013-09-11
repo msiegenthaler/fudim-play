03adc96311fa1084ba963ad804b5bff1c124b328
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-11 21:41:35 +0200

    fix dimension and dimensions view.

--- app/controllers/Dimensions.scala
+++ app/controllers/Dimensions.scala
@@ -9,13 +9,13 @@ import support.DomainAction
 import support.DimensionAction
 
 object Dimensions extends Controller {
-  def index(domain: String) = DomainAction(domain) { domain ⇒
-    Ok(views.html.dimensions(domain.dimensions, addForm))
+  def index(domainName: String) = DomainAction(domainName) { domain ⇒
+    Ok(views.html.dimensions(domainName, domain.dimensions, addForm))
   }
 
   def add(domainName: String) = DomainAction(domainName).on(domain ⇒ { implicit request ⇒
     addForm.bindFromRequest.fold(
-      errors ⇒ BadRequest(views.html.dimensions(domain.dimensions, errors)),
+      errors ⇒ BadRequest(views.html.dimensions(domainName, domain.dimensions, errors)),
       name ⇒ {
         domain.dimensionRepo.create(name)
         Redirect(routes.Dimensions.index(domainName))
@@ -23,12 +23,12 @@ object Dimensions extends Controller {
   })
 
   def get(domainName: String, name: String) = DimensionAction(domainName, name) { dimension ⇒
-    Ok(views.html.dimension(dimension, dimension.values, addValueForm))
+    Ok(views.html.dimension(domainName, dimension, dimension.values, addValueForm))
   }
 
   def addValue(domainName: String, name: String) = DimensionAction(domainName, name).on(dimension ⇒ { implicit request ⇒
     addValueForm.bindFromRequest.fold(
-      errors ⇒ BadRequest(views.html.dimension(dimension, dimension.values, errors)),
+      errors ⇒ BadRequest(views.html.dimension(domainName, dimension, dimension.values, errors)),
       value ⇒ {
         dimension.add(value)
         Redirect(routes.Dimensions.get(domainName, name))

4b91d8f83fe723e49610399b717ebecdd7d6d840
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-15 23:07:57 +0200

    allow to add new domains.

--- app/controllers/Domains.scala
+++ app/controllers/Domains.scala
@@ -11,10 +11,21 @@ import models.playbinding.DomainRepo
 
 object Domains extends Controller {
   def index = Action {
-    Ok(views.html.domains(DomainRepo.all))
+    Ok(views.html.domains(DomainRepo.all, addForm))
   }
 
   def get(name: String) = DomainAction(name) { domain ⇒
     Ok(views.html.domain(domain))
   }
+
+  def add = Action { implicit request ⇒
+    addForm.bindFromRequest.fold(
+      errors ⇒ BadRequest(views.html.domains(DomainRepo.all, errors)),
+      name ⇒ {
+        DomainRepo.create(name)
+        Redirect(routes.Domains.index)
+      })
+  }
+
+  val addForm = Form("name" -> nonEmptyText)
 }

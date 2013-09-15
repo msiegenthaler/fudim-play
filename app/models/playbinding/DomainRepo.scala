af8d1c403adbffe2359ec740241a1ea667f257f5
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-15 21:47:54 +0200

    implement PlayDomainRepo.

--- app/models/playbinding/DomainRepo.scala
+++ app/models/playbinding/DomainRepo.scala
@@ -5,13 +5,24 @@ import cube._
 import db._
 import dbcube.DatabaseCubeRepo
 
-object DomainRepo extends FudimDomainRepo {
-  private object DimensionRepo extends DatabaseDimensionRepo with PlayDatabaseRepo
-  private object DbCubeRepo extends DatabaseCubeRepo with PlayDatabaseRepo
-  private object FactRepo extends DatabaseFactRepo with PlayDatabaseRepo {
-    override def databaseCubeRepo = DbCubeRepo
-    override def jsonCubeMapperRepo = new JsonCubeMapperRepository {
-      override val mappers = DbCubeRepo.json :: CubeDecorator.json(JsonMappers.decorator, this) :: Nil
+object DomainRepo extends DatabaseDomainRepo with PlayDatabaseRepo {
+  override protected def dimensionRepo(d: DomainId) = {
+    new DatabaseDimensionRepo with PlayDatabaseRepo {
+      override def domain = d
+    }
+  }
+  override protected def factRepo(d: DomainId) = {
+    val dims = dimensionRepo(d)
+    new DatabaseFactRepo with PlayDatabaseRepo {
+      override def domain = d
+      override val databaseCubeRepo = {
+        new DatabaseCubeRepo with PlayDatabaseRepo {
+          override def dimension(name: String) = dims.get(name)
+        }
+      }
+      override val jsonCubeMapperRepo = new JsonCubeMapperRepository {
+        override val mappers = databaseCubeRepo.json :: CubeDecorator.json(JsonMappers.decorator, this) :: Nil
+      }
     }
   }
 }

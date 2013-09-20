f71c8b2415020b050ef17cf17f8d4ba4048a8bd0
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-20 17:14:53 +0200

    basic structure of FactsTable.

---
+++ app/controllers/FactsTable.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api.mvc._
+import support.DomainAction
+
+object FactsTable extends Controller {
+
+  def index(domainName: String) = DomainAction(domainName) { domain =>
+    domain.dimensions.headOption.map { dim =>
+      Redirect(routes.FactsTable.show(domainName, dim.name, Nil))
+    }.getOrElse(NotFound)
+  }
+
+  def show(domainName: String, dimensionName: String, factNames: List[String]) = DomainAction(domainName) { domain =>
+    domain.dimensionRepo.get(dimensionName).map { dimension =>
+      val factOpts = factNames.map(domain.factRepo.get)
+      if (factOpts.contains(None)) NotFound
+      else {
+        val facts = factOpts.map(_.get)
+        Ok("Check")
+      }
+    }.getOrElse(NotFound)
+  }
+}

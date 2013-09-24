7020060b0421ccceb3607c9d0cab3d78bdbbb372
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-24 22:43:31 +0200

    Add bloom filter implementation.

--- app/support/BloomFilter.scala
+++ app/support/BloomFilter.scala
@@ -1,5 +1,62 @@
 package support
 
+import scala.collection.immutable.BitSet
+import scala.util.hashing.MurmurHash3
+
+// Based on the implementation by Ilya Sterin
+
+case class BloomFilter private(bits: BitSet, config: BloomFilterConfig) {
+  def +(data: Array[Byte]): BloomFilter = {
+    // see "Less Hashing, Same Performance" by Adam Kirsch and Michael Mitzenmacher
+    val hash1 = MurmurHash3.bytesHash(data, 0)
+    val hash2 = MurmurHash3.bytesHash(data, hash1)
+    val capacity = config.capacity
+    val newBits = bits ++ (0 until config.hashCount).map(i => Math.abs((hash1 + i * hash2) % capacity))
+    new BloomFilter(newBits, config)
+  }
+
+  def ++(data: TraversableOnce[Array[Byte]]) = data.foldLeft(this)(_ + _)
+  def ++(other: BloomFilter) = union(other)
+
+  def union(other: BloomFilter) = {
+    require(config == other.config, "Same configuration required")
+    new BloomFilter(bits | other.bits, config)
+  }
+
+  def intersect(other: BloomFilter) = {
+    require(config == other.config, "Same configuration required")
+    new BloomFilter(bits & other.bits, config)
+  }
+
+
+  /** True if this filter might contain the element. False if the element is not contained for sure. */
+  def maybeContains(data: Array[Byte]): Boolean = {
+    val hash1 = MurmurHash3.bytesHash(data, 0)
+    val hash2 = MurmurHash3.bytesHash(data, hash1)
+    val capacity = config.capacity
+    (0 until config.hashCount).forall { i =>
+      val hash = Math.abs((hash1 + i * hash2) % capacity)
+      bits.contains(hash)
+    }
+  }
+
+  /** True if the filter does not contain the element (for sure). */
+  def notContains(data: Array[Byte]): Boolean = !maybeContains(data)
+
+  def approxNumberOfItems = {
+    -(config.capacity.toDouble * Math.log(1d - (bits.size.toDouble / config.capacity))) / config.hashCount
+  }
+
+  override def toString = s"BloomFilter($config)"
+}
+object BloomFilter {
+  def apply(config: BloomFilterConfig): BloomFilter = BloomFilter(BitSet.empty, config)
+  def apply(expectedItemCount: Int, falsePositiveProbability: Double): BloomFilter = {
+    val config = BloomFilterConfig.forFalsePositives(expectedItemCount, falsePositiveProbability)
+    BloomFilter(config)
+  }
+}
+
 case class BloomFilterConfig(/** Number of bits in the filter. */
                              capacity: Int,
 
--- test/support/BloomFilterSpec.scala
+++ test/support/BloomFilterSpec.scala
@@ -1,6 +1,8 @@
 package support
 
+import java.nio.ByteBuffer
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 class BloomFilterSpec extends Specification {
   "BloomFilterConfig with capacity 1438 and 10 hashes" should {
@@ -30,6 +32,69 @@ class BloomFilterSpec extends Specification {
         BloomFilterConfig.forFalsePositives(n, 0.01d).falsePositives(n) must beCloseTo(0.01d, 1E-3)
         BloomFilterConfig.forFalsePositives(n, 0.001d).falsePositives(n) must beCloseTo(0.001d, 1E-4)
       }
+    }
+  }
+
+  trait xvalues extends Scope {
+    def value(d: Int) = ByteBuffer.allocate(4).putInt(d).array
+  }
+  trait hundred extends xvalues {
+    val filter = BloomFilter(100, 0.001d) ++ (1 to 100).map(value)
+  }
+  trait ten extends xvalues {
+    val filter = BloomFilter(100, 0.001d) ++ (1 to 10).map(value)
+  }
+  trait union extends ten {
+    val filter2 = BloomFilter(100, 0.001d) ++ (8 to 15).map(value)
+    val union = filter union filter2
+  }
+  trait intersect extends ten {
+    val filter2 = BloomFilter(100, 0.001d) ++ (8 to 15).map(value)
+    val intersect = filter intersect filter2
+  }
+
+  "BloomFilter that contains the items 1..100" should {
+    "return true maybeContains 1..100" in new hundred {
+      (1 to 100).map(value).map { v =>
+        filter.maybeContains(v) must beTrue
+      }
+    }
+    "have not have more than 120 false positive in 101 to 10000" in new hundred {
+      val count = (101 to 100000).map(value).filter(filter.maybeContains).size
+      count must beLessThanOrEqualTo(120)
+    }
+    "have approx item count of 100+-2" in new hundred {
+      filter.approxNumberOfItems must beCloseTo(100d, 2)
+    }
+  }
+
+  "BloomFilter that contains [1..10] unioned with [8..15]" should {
+    "contain approx 15 items" in new union {
+      union.approxNumberOfItems must beCloseTo(15d, 1)
+    }
+    "return true for maybeContains 1..15" in new union {
+      (1 to 15).map(value).map { v =>
+        union.maybeContains(v) must beTrue
+      }
+    }
+    "have not have more than 5 false positive in 16 to 10000" in new union {
+      val count = (16 to 100000).map(value).filter(union.maybeContains).size
+      count must beLessThanOrEqualTo(5)
+    }
+  }
+
+  "BloomFilter that contains [1..10] intersected with [8..15]" should {
+    "contain approx 3 items" in new intersect {
+      intersect.approxNumberOfItems must beCloseTo(3d, 1)
+    }
+    "return true for maybeContains 8..10" in new intersect {
+      (8 to 10).map(value).map { v =>
+        intersect.maybeContains(v) must beTrue
+      }
+    }
+    "have not have more than 5 false positive in 0..7 and 11 to 10000" in new intersect {
+      val count = ((0 to 7) ++ (11 to 100000)).map(value).filter(intersect.maybeContains).size
+      count must beLessThanOrEqualTo(5)
     }
   }
 }

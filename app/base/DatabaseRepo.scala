f16bae4f848072d4abc37bce0ec866e7adf0916a
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-30 21:14:09 +0200

    Better impl of SqlDatabase, get rid of DatabaseRepo.

--- app/base/DatabaseRepo.scala
+++
@@ -1,7 +0,0 @@
-package base
-
-import java.sql.Connection
-
-trait DatabaseRepo {
-  protected def withConnection[A](f: Connection ⇒ A): A
-}
--- app/base/SqlDatabase.scala
+++ app/base/SqlDatabase.scala
@@ -2,13 +2,7 @@ package base
 
 import java.sql.Connection
 
-trait SqlDatabase extends TransactionState {
-  protected def connection: Connection
-}
-
-object SqlDatabase extends TransactionalRessource {
-  def transaction[A](b: Connection => A) = execute {
-    case db: SqlDatabase => b(db.connection)
-    case t => throw new IllegalStateException(s"Transaction $t does not support SqlDatabase")
-  }
+trait SqlDatabase extends TransactionalRessource {
+  def transaction[A](b: Connection => A): Transaction[A]
+  def readOnly[A](b: Connection => A): A
 }

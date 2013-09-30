c50520fb8fd221470af6b30ef203fb6002301f75
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-09-30 12:42:46 +0200

    TransactionRessource need TransactionState.

--- app/base/Transaction.scala
+++ app/base/Transaction.scala
@@ -37,7 +37,7 @@ object Transaction {
 trait TransactionState
 
 /** A ressource that can take part in a transaction. */
-trait TransactionalRessource[T] {
+trait TransactionalRessource[T <: TransactionState] {
   /** Provides access to the transaction state. */
   protected def execute[A](f: T => A): Transaction[A] = Transaction.on {
     case t: T => f(t)

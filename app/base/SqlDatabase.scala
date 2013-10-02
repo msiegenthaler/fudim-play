61791225dee74f90023be4eddbee9f3470195c6c
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-02 15:34:22 +0200

    Change the way read only txs work.

--- app/base/SqlDatabase.scala
+++ app/base/SqlDatabase.scala
@@ -3,6 +3,6 @@ package base
 import java.sql.Connection
 
 trait SqlDatabase extends TransactionalRessource {
-  def transaction[A](b: Connection => A): Transaction[A]
-  def readOnly[A](b: Connection => A): A
+  def inTransaction[A](b: Connection => A): Transaction[A]
+  def readOnly[A](tx: Transaction[A]): A
 }
--- app/support/AnormDb.scala
+++ app/support/AnormDb.scala
@@ -7,12 +7,19 @@ import base._
 
 /** Helper for database handling with anorm. */
 class AnormDb(db: SqlDatabase) {
-  private def apply[A](f: Connection => A) = db.inTransaction(f).tx
+  def single[A](sql: Sql, parser: RowParser[A]) = appl(sql.single(parser)(_))
+  def select[A](sql: Sql, parser: ResultSetParser[A]) = appl(sql.as(parser)(_))
 
-  def single[A](sql: Sql, parser: RowParser[A]) = apply(sql.single(parser)(_))
-  def select[A](sql: Sql, parser: ResultSetParser[A]) = apply(sql.as(parser)(_))
-  def insert[A](sql: Sql, parser: ResultSetParser[A] = scalar[Long].singleOpt) = apply(sql.executeInsert(parser)(_))
-  def update(sql: Sql) = apply(sql.executeUpdate()(_))
+  object notx {
+    private val o = AnormDb.this
+    def single[A](sql: Sql, parser: RowParser[A]): A = db.readOnly(o.single(sql, parser).m)
+    def select[A](sql: Sql, parser: ResultSetParser[A]): A = db.readOnly(o.select(sql, parser).m)
+  }
+
+  def insert[A](sql: Sql, parser: ResultSetParser[A] = scalar[Long].singleOpt) = appl(sql.executeInsert(parser)(_))
+  def update(sql: Sql) = appl(sql.executeUpdate()(_))
   def delete(sql: Sql) = update(sql)
-  def execute(sql: Sql) = apply(sql.execute()(_))
+  def execute(sql: Sql) = appl(sql.execute()(_))
+
+  private def appl[A](f: Connection => A) = db.inTransaction(f).tx
 }

d5a10df02908f6afc403fb1ef2715a05253f940a
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-11 17:59:35 +0200

    Add test to see if application boots.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -7,9 +7,15 @@ import play.api.test.Helpers._
 
 class ApplicationSpec extends Specification {
   "Application" should {
+    "boot" in {
+      running(FakeApplication()) {
+        val result  = route( FakeRequest( GET, "/")).get
+        status(result) must equalTo(303)
+      }
+    }
     "send 404 on a bad request" in {
       running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone
+        route(FakeRequest(GET, "/does-not-exist")) must beNone
       }
     }
   }

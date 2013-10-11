a69b378c3946b2005492b6d8d594ac0ace9cc469
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-11 18:26:38 +0200

    Refactor test to use its run method.

--- test/base/TransactionSpec.scala
+++ test/base/TransactionSpec.scala
@@ -32,6 +32,8 @@ class TransactionSpec extends Specification {
 
     case class Txs(value: Int = 0) extends TransactionState
 
+    def run[A](tx: Transaction[A], initialValue: Int = 0) = tx.run(Txs(initialValue))
+
     def assertionAction(p: Txs => Unit): Transaction[Unit] = Transaction.on {
       case t: Txs => p(t); (t, ())
     }
@@ -40,17 +42,17 @@ class TransactionSpec extends Specification {
   "Transaction.pure" should {
     "return the unchanged state in run" in new txs {
       val a = Transaction.pure(10)
-      val (s1, _) = a.run(Txs(10))
+      val (s1, _) = run(a, 10)
       s1 must_== Txs(10)
-      val (s2, _) = a.run(Txs(12))
+      val (s2, _) = run(a, 12)
       s2 must_== Txs(12)
     }
     "return the value to run" in new txs {
       List(10, 0, "Hallo", ()).foreach { v =>
         val a = Transaction.pure(v)
-        val (_, v1) = a.run(Txs(1))
+        val (_, v1) = run(a, 1)
         v1 must_== v
-        val (_, v2) = a.run(Txs(2))
+        val (_, v2) = run(a, 2)
         v2 must_== v
       }
     }
@@ -59,32 +61,32 @@ class TransactionSpec extends Specification {
   "Transaction.flatMap" should {
     "execute the first action" in new txs {
       val tx = Res.set(3) >> Res.get
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 3
       Res.value must_== 3
     }
     "execute the second action" in new txs {
       val tx = Res.increment >> Res.set(10)
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 10
       Res.value must_== 10
     }
     "execute both actions" in new txs {
       val tx = Res.increment >> Res.increment
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 2
       Res.value must_== 2
     }
     "pass the result of the first action to the second" in new txs {
       def addTwoAndSet(to: Int) = Res.set(to + 2)
       val tx = Transaction.pure(111) >>= addTwoAndSet
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 113
       Res.value must_== 113
     }
     "pass the txstate of the first action to the second" in new txs {
       val tx = Res.set(123) >> Res.setStateToThis >> Res.set(10)
-      val (Txs(tv), v) = tx.run(Txs())
+      val (Txs(tv), v) = run(tx)
       tv must_== 123
       v must_== 10
     }
@@ -93,12 +95,12 @@ class TransactionSpec extends Specification {
   "Transaction.<*" should {
     "execute both actions" in new txs {
       val tx = Res.increment <* Res.increment
-      tx.run(Txs())
+      run(tx)
       Res.value must_== 2
     }
     "return the result of the first action" in new txs {
       val tx = Res.set(10) <* Res.increment
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 10
       Res.value must_== 11
     }
@@ -106,12 +108,12 @@ class TransactionSpec extends Specification {
   "Transaction.*>" should {
     "execute both actions" in new txs {
       val tx = Res.increment *> Res.increment
-      tx.run(Txs())
+      run(tx)
       Res.value must_== 2
     }
     "return the result of the second action" in new txs {
       val tx = Res.set(10) *> Res.increment
-      val (_, v) = tx.run(Txs())
+      val (_, v) = run(tx)
       v must_== 11
       Res.value must_== 11
     }

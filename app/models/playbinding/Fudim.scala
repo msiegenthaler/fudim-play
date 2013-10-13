f5661230ff761c425a843b56df1137d8bedccaae
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-13 20:43:12 +0200

    Move execReadOnly up to Fudim.

--- app/models/playbinding/Fudim.scala
+++ app/models/playbinding/Fudim.scala
@@ -11,15 +11,19 @@ import play.api.Logger
 object Fudim {
   def exec[A](tx: Transaction[A]): A = {
     val state = TxState.full
-    val decTx = tx <* onEndOfTx
+    val decTx = tx <* Db.commit
     val (s, r) = decTx.run(state)
     cleanupTx(s)
     r.fold(throw _, identity)
   }
   def execTx[A](tx: => A@tx): A = exec(tx.transaction)
 
-  /** Executed at the end of the transaction (inside the transaction, only if transaction is successful). */
-  private def onEndOfTx = Db.commit
+  def execReadOnly[A](tx: Transaction[A]): A = {
+    val (s, result) = (tx <* Db.rollback).run(TxState.readOnly)
+    cleanupTx(s)
+    result.fold(throw _, identity)
+  }
+  def execReadOnlyTx[A](tx: => A@tx): A = execReadOnly(tx.transaction)
 
   /** Executed after the transaction. */
   private def cleanupTx(s: TransactionState) = Db.cleanupTx(s)
@@ -45,7 +49,7 @@ object Fudim {
       def top = get.headOption.map(_._2)
     }
 
-    def inTransaction[A](b: Connection => A) = execute {
+    override def inTransaction[A](b: Connection => A) = execute {
       case s@TxState(_, _, Some(conn)) =>
         (s, b(conn))
       case s@TxState(_, false, None) =>
@@ -71,15 +75,7 @@ object Fudim {
       case _ => throw new AssertionError("Unsupported TransactionState: Does not implement Db")
     }
 
-    def readOnly[A](tx: Transaction[A]) = {
-      //If a transaction is running on the thread, then reuse the tx's connection
-      // else we get deadlocks and other not so nice things.
-      val state = TxState.readOnly
-      val (s, result) = (tx <* rollback).run(state)
-      Fudim.cleanupTx(s)
-      result.fold(throw _, identity)
-    }
-
+    override def readOnly[A](tx: Transaction[A]) = Fudim.execReadOnly(tx)
 
     private[Fudim] def rollback = withExistingConnection(_.rollback())
     private[Fudim] def commit = withExistingConnection(_.commit())

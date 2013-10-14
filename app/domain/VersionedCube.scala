78860e6e83d8f3c053fd4b96de6ffa02add24d55
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-14 21:24:17 +0200

    Remove Version trait.

--- app/domain/VersionedCube.scala
+++ app/domain/VersionedCube.scala
@@ -2,22 +2,17 @@ package domain
 
 import cube._
 
-trait Version extends Ordered[Version] {
-  def newerThan(other: Version) = this > other
-  def olderThan(other: Version) = this < other
-}
-
 /** Cube that tracks a version for each value. */
-trait VersionedCube[T, +V <: Version] extends Cube[T] {
-  type Self <: VersionedCube[T, V]
+trait VersionedCube[T, Version] extends Cube[T] {
+  type Self <: VersionedCube[T, Version]
 
   /** Version of the newest element in this cube (view). Slice, dice and after are respected. */
-  def version: V
+  def version: Version
 
   /** Version the value at that point. Even points with no value do have a version. */
   def version(p: Point): Version = slice(p).version
-
   /**
+
    * View of the cube that only contains values newer than the specified version.
    * Especially usefull with in combination with sparse (after(x).sparse returns all values that were changed since x).
    */

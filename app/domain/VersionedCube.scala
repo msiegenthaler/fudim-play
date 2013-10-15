80cb6019c02fc31aed61bc21f525216eff159261
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-15 22:03:34 +0200

    Strip down VersionedCube.

--- app/domain/VersionedCube.scala
+++ app/domain/VersionedCube.scala
@@ -11,10 +11,4 @@ trait VersionedCube[T, Version] extends Cube[T] {
 
   /** Version the value at that point. Even points with no value do have a version. */
   def version(p: Point): Version = slice(p).version
-  /**
-
-   * View of the cube that only contains values newer than the specified version.
-   * Especially usefull with in combination with sparse (after(x).sparse returns all values that were changed since x).
-   */
-  def after(v: Version): Self
 }

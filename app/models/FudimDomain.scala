a903d20fc6aaebecf3e2978a72608ad0fe7f5b5e
Mario Siegenthaler <msiegenthaler@gmx.net>
2013-10-27 20:07:54 +0100

    Merge FudimDomain and Domain.

--- app/models/FudimDomain.scala
+++ app/models/FudimDomain.scala
@@ -1,22 +1,35 @@
 package models
 
 import base._
+import cube._
 import domain._
 
 case class DomainId(id: Long)
 
-trait FudimDomain extends Domain {
+trait FudimDomain {
   def id: DomainId
+
+  def name: String
+
   /** Version of the structure of the domain (data is ignored). */
   def version: Version
 
   def dimensionRepo: FudimDimensionRepo
-  def factRepo: FudimFactRepo
-
   def dimensions = dimensionRepo.all.toSet
   def dimension(name: String) = dimensionRepo.get(name)
+
+  def factRepo: FudimFactRepo
   def facts = factRepo.all.toSet
   def fact(name: String) = factRepo.get(name)
+
+  def cubes: Cubes = DomainCubes(this)
+}
+
+private case class DomainCubes(domain: FudimDomain) extends Cubes {
+  override def refs = domain.facts.map(f ⇒ CubeRef(f.name, f.dataType))
+  override def get[T](ref: CubeRef[T]) = {
+    domain.fact(ref.name).filter(_.dataType == ref.dataType).map(_.data.asInstanceOf[Cube[T]])
+  }
 }
 
 trait FudimDomainRepo {
